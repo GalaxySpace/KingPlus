8483eff85b523cc89ccf32979abd4fe0019469e7
GalaxySpace <49269418+GalaxySpace@users.noreply.github.com>
2024-07-23 18:34:30 +0800

    Update VPNPlus.js

--- VPNPlus.js
+++ VPNPlus.js
@@ -22,6 +22,7 @@ var obj = JSON.parse(body);
 const tmp1 = '/receipt/apple';
 
 if (url.indexOf(tmp1) != -1) {
-	obj.ExpireTime = 992503620603;
+	obj.ExpireTime = '992503620603';
+	body = JSON.stringify(obj);
 }
 $done({body});
